762e844ddc4b85c1dfd37b5fdeaab3d9062f9dac
Lukas Gerhold <lukger1999@gmail.com>
2022-10-03 18:49:19 +0200

    :construction: Started with date selector component

--- src/lib/components/day-selector.svelte
+++ src/lib/components/day-selector.svelte
@@ -1,3 +1,39 @@
 <script lang="ts">
+	export let selectedDate: Date;
+
+	const margin = 3;
+
+    let dates: Date[] = []
+
+    $:{
+        let dateFrom = new Date(selectedDate.valueOf() - margin * 1000 * 3600 * 24);
+        let dateTo = new Date(selectedDate.valueOf() + margin * 1000 * 3600 * 24);
     
+        dates = [];
+
+        for (let date = dateFrom; date <= dateTo; date.setDate(date.getDate() + 1)) {
+            const cloned = new Date(date.valueOf());
+            dates.push(cloned);
+        }
+    }
+
+
 </script>
+
+<div class="flex flex-row gap-4">
+	{#each dates as date}
+		<div class="w-16 h-16 grid place-items-center border-2 rounded-xl bg-white {date.getDate() === selectedDate.getDate() ? "selected" : "unselected"}" on:click={() => selectedDate = date}>
+			<p class="text-2xl select-none">{date.getDate()}</p>
+		</div>
+	{/each}
+</div>
+
+<style lang="scss">
+    .selected {
+        @apply border-red-300;
+    }
+
+    .unselected {
+        @apply border-slate-600;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import ArtworkContainer from '$lib/components/artworkContainer.svelte';
+	import DaySelector from '$lib/components/day-selector.svelte';
 	import { token } from "$lib/stores/token";
 	import type { Game } from '@prisma/client';
 	import { onMount } from "svelte";
@@ -25,6 +26,7 @@
 </script>
 
 <div class="h-screen w-screen grid place-items-center">
+	<DaySelector selectedDate={new Date(2022, 10, 3)}></DaySelector>
 	<ArtworkContainer imgUrl={$game?.imgUrl} guessCount={1} />
 	{$token}
 </div>
